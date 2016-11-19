8a4ede726796567fc63721e787907e3fe8a4619d
hasangirit <hsngirit@gmail.com>
2016-11-19 18:27:45 +0300

    onmouseover

--- public/default.js
+++ public/default.js
@@ -144,6 +144,8 @@
             position: serverGame.board ? serverGame.board : 'start',
             onDragStart: onDragStart,
             onDrop: onDrop,
+            onMouseoutSquare: onMouseoutSquare,
+            onMouseoverSquare: onMouseoverSquare,
             onMoveEnd: onMoveEnd,
             onSnapEnd: onSnapEnd,
             onDragMove: onDragMove,
@@ -157,6 +159,22 @@
             boardEl.find('.square-55d63')
             .removeClass('highlight-' + color);
         };
+
+        var removeGreySquares = function() {
+          $('#board .square-55d63').css('background', '');
+        };
+
+        var greySquare = function(square) {
+          var squareEl = $('#board .square-' + square);
+          
+          var background = '#a9a9a9';
+          if (squareEl.hasClass('black-3c85d') === true) {
+            background = '#696969';
+          }
+
+          squareEl.css('background', background);
+        };
+
       // do not pick up pieces if the game is over
       // only pick up pieces for the side to move
       var onDragStart = function(source, piece, position, orientation) {
@@ -183,6 +201,7 @@
         if (possibleMoves.length === 0){console.log("no more possible moves!");return;};
       
       var onDrop = function(source, target) {
+        removeGreySquares();
         // see if the move is legal
         var move = game.move({
           from: source,
@@ -206,6 +225,31 @@
         boardEl.find('.square-' + squareToHighlight)
         .addClass('highlight-black');
       };
+
+
+      var onMouseoverSquare = function(square, piece) {
+      // get list of possible moves for this square
+      var moves = game.moves({
+        square: square,
+        verbose: true
+      });
+
+      // exit if there are no moves available for this square
+      if (moves.length === 0) return;
+
+      // highlight the square they moused over
+      greySquare(square);
+
+      // highlight the possible squares for this piece
+      for (var i = 0; i < moves.length; i++) {
+        greySquare(moves[i].to);
+      }
+      };
+
+      var onMouseoutSquare = function(square, piece) {
+        removeGreySquares();
+      };
+
       
       // update the board position after the piece snap 
       // for castling, en passant, pawn promotion
